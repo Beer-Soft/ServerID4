71095f13712d738ea3a0834694e3b3e57ae34105
Diego Luna <juanperez@MacBook-Pro-de-juan.local>
2019-09-12 23:24:11 -0500

    primer commit para el proyecto

---
+++ BeerSoft.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BeerSoft.WebApi.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "ApiDiego" };
+        }
+
+       
+    }
+}
